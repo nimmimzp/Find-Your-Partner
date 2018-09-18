d8ae14f15575ed197d6fe92baa291a9cbfc1402c
Nimmi Verma <nimmimzp26@gmail.com>
2018-09-18 15:08:28 +0530

    add sidedrawer

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { Navigation } from "react-native-navigation";
-
+import { Provider } from "react-redux";
 import AuthScreen from "./src/screens/Auth/Auth";
 import UserProfile from './src/screens/UserProfile/UserProfile';
 import AllUsers from './src/screens/AllUsers/AllUsers';
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import SideDrawer from "./src/screens/SideDrawer/SideDrawer";
+import configureStore from "./src/store/configureStore";
+
+const store = configureStore();
 // Register Screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.UserProfile", () => UserProfile);
-Navigation.registerComponent("awesome-places.AllUsers", () => AllUsers);
+Navigation.registerComponent(
+  "awesome-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.UserProfile",
+  () => UserProfile,
+  store,
+  Provider
+);
+Navigation.registerComponent("awesome-places.AllUsers", 
+  () => AllUsers,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "awesome-places.PlaceDetailScreen",
+  () => PlaceDetailScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.SideDrawer",
+  () => SideDrawer
+);
 
 // Start a App
 Navigation.startSingleScreenApp({
